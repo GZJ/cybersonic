17681d59c4b4706a37ad2815469f7972f08633ea
gzj <gzj00@outlook.com>
2024-11-27 10:07:01 +0800

    refactor: add structured logging with request name, id, message, and timestamp for better tracking of request sources

--- cmd/cybersonic/main.go
+++ cmd/cybersonic/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
-	"net/url"
 	"os"
-
+	"time"
 	flag "github.com/spf13/pflag"
 )
 
@@ -14,48 +15,107 @@ var address string
 
 func main() {
 	var (
-		sfx string
+		sfx     string
+		name    string
+		id      int
+		message string
+		verbose bool
 	)
-
 	flag.StringVar(&sfx, "sfx", "", "sfx name")
+	flag.StringVar(&name, "name", "", "name to send")
+	flag.IntVar(&id, "id", 0, "id to send")
+	flag.StringVar(&message, "message", "", "message to send")
 	flag.StringVar(&address, "address", "127.0.0.1:49161", "server address")
-
+	flag.BoolVar(&verbose, "verbose", false, "enable verbose output")
 	flag.Parse()
-
-	a := flag.Args()
-	if len(a) > 0 {
-		sfx = a[0]
-		getSfx(sfx)
+	
+	args := flag.Args()
+	if len(args) > 0 {
+		sfx = args[0]
+	}
+	if len(args) > 1 {
+		name = args[1]
+	}
+	if len(args) > 2 {
+		id, _ = fmt.Sscanf(args[2], "%d", &id)
+	}
+	if len(args) > 3 {
+		message = args[3]
+	}
+	
+	if sfx != "" {
+		postSfx(sfx, name, id, message, verbose)
 	} else {
 		getAll()
 	}
 }
 
-func getSfx(name string) {
-	params := url.Values{}
-	params.Add("name", name)
-
-	url := fmt.Sprintf("http://%s/sfx?%s", address, params.Encode())
-
-	resp, err := http.Get(url)
+func postSfx(sfxName, name string, id int, message string, verbose bool) {
+	body := map[string]interface{}{
+		"name":      name,
+		"id":        id,
+		"message":   message,
+		"timestamp": time.Now().Format(time.RFC3339Nano),
+	}
+	jsonData, err := json.Marshal(body)
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("Error encoding JSON:", err)
+		return
+	}
+	url := fmt.Sprintf("http://%s/sfx?name=%s", address, sfxName)
+	
+	if verbose {
+		fmt.Println("Sending POST request:")
+		fmt.Printf("URL: %s\n", url)
+		fmt.Println("Headers:")
+		fmt.Println("  Content-Type: application/json")
+		fmt.Println("Body:")
+		fmt.Printf("  SFX Name: %s\n", sfxName)
+		fmt.Printf("  Name: %s\n", name)
+		fmt.Printf("  ID: %d\n", id)
+		fmt.Printf("  Message: %s\n", message)
+		fmt.Printf("  Timestamp: %s\n", body["timestamp"])
+		fmt.Println("JSON Payload:")
+		fmt.Println(string(jsonData))
+		fmt.Println("-----------------------------------------------------------")
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
 		return
 	}
 	defer resp.Body.Close()
-
+	if verbose {
+		fmt.Println("Response:")
+		fmt.Printf("Status: %s\n", resp.Status)
+		fmt.Println("Headers:")
+		for k, v := range resp.Header {
+			fmt.Printf("  %s: %v\n", k, v)
+		}
+		fmt.Println("Body:")
+		fmt.Println("-----------------------------------------------------------")
+	}
+	_, err = io.Copy(os.Stdout, resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+	}
 }
 
 func getAll() {
 	url := fmt.Sprintf("http://%s/all", address)
-
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 	defer resp.Body.Close()
-
 	_, err = io.Copy(os.Stdout, resp.Body)
 	if err != nil {
 		fmt.Println("Error:", err)
--- cmd/cybersonicd/main.go
+++ cmd/cybersonicd/main.go
@@ -1,32 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
-	"log"
+	"log/slog"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
-
-	e "github.com/gzj/cybersonic/assets"
 
 	"github.com/getlantern/systray"
 	"github.com/gopxl/beep"
 	"github.com/gopxl/beep/speaker"
 	"github.com/gopxl/beep/wav"
+	e "github.com/gzj/cybersonic/assets"
 )
 
 // -------------------------------- init --------------------------------
-var P map[string]*SoundPlayer
-var Sfx map[string]io.ReadCloser
-var SfxFiles []string
+var (
+	P        map[string]*SoundPlayer
+	Sfx      map[string]io.ReadCloser
+	SfxFiles []string
+	logger   *slog.Logger
+	logLevel *slog.LevelVar
+)
 
 func init() {
+	logLevel = &slog.LevelVar{}
+	logLevel.Set(slog.LevelInfo)
+
 	P = make(map[string]*SoundPlayer)
 	Sfx = make(map[string]io.ReadCloser)
+
 	files, err := e.Sfx.ReadDir("sfx")
 	if err != nil {
+		slog.Error("Failed to read sfx directory", "error", err)
 		panic(err)
 	}
 
@@ -35,12 +45,13 @@ func init() {
 		SfxFiles = append(SfxFiles, name)
 		f, err := e.Sfx.Open("sfx" + "/" + name)
 		if err != nil {
-			log.Println(err)
+			slog.Error("Failed to open sfx file", "filename", name, "error", err)
+			continue
 		}
 		Sfx[name] = f
 		P[name], err = NewSoundPlayer(f)
 		if err != nil {
-			log.Println(err)
+			slog.Error("Failed to create sound player", "filename", name, "error", err)
 		}
 	}
 }
@@ -55,13 +66,11 @@ type SoundPlayer struct {
 func NewSoundPlayer(r io.ReadCloser) (*SoundPlayer, error) {
 	streamer, format, err := wav.Decode(r)
 	if err != nil {
-		log.Println(err)
+		logger.Error("Failed to decode WAV file", "error", err)
 	}
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-
 	buffer := beep.NewBuffer(format)
 	buffer.Append(streamer)
-
 	return &SoundPlayer{
 		reader:   r,
 		streamer: streamer,
@@ -80,19 +89,108 @@ func (sp *SoundPlayer) Close() {
 }
 
 // -------------------------------- server --------------------------------
+type EventType int
+
+const (
+	EventTypeUnknown EventType = iota
+	EventTypeListAll
+	EventTypePlaySound
+)
+
+func (et EventType) String() string {
+	switch et {
+	case EventTypeListAll:
+		return "list_all"
+	case EventTypePlaySound:
+		return "play_sound"
+	default:
+		return "unknown"
+	}
+}
+
 func HandlerAll(w http.ResponseWriter, r *http.Request) {
+	logger.Info("Sound files list",
+		"method", r.Method,
+		"event_type", EventTypeListAll.String(),
+		"remote_addr", r.RemoteAddr)
+
 	for _, file := range SfxFiles {
 		fmt.Fprintf(w, "%s\n", file)
 	}
 }
+
 func HandlerSfx(w http.ResponseWriter, r *http.Request) {
-	values := r.URL.Query()
-	name := values.Get("name")
-	n := name + ".wav"
+	sfxName := r.URL.Query().Get("name")
+	n := sfxName + ".wav"
+
+	var payload map[string]interface{}
+	err := json.NewDecoder(r.Body).Decode(&payload)
+
+	name := ""
+	var id int
+	message := ""
+
+	if err != nil {
+		logger.Error("Request body decoding failed",
+			"error", err,
+			"sfx_name", sfxName,
+			"event_type", EventTypePlaySound.String(),
+			"remote_addr", r.RemoteAddr)
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	if nameVal, ok := payload["name"]; ok {
+		name, _ = nameVal.(string)
+	}
+
+	if idVal, ok := payload["id"]; ok {
+		switch v := idVal.(type) {
+		case float64:
+			id = int(v)
+		case int:
+			id = v
+		case string:
+			idInt, err := strconv.Atoi(v)
+			if err != nil {
+				logger.Warn("Invalid ID format",
+					"id", v,
+					"event_type", EventTypePlaySound.String(),
+					"error", err)
+			} else {
+				id = idInt
+			}
+		}
+	}
+
+	if messageVal, ok := payload["message"]; ok {
+		message, _ = messageVal.(string)
+	}
+
+	logger.Debug("Sound play request details",
+		"sfx_name", sfxName,
+		"sound_file", n,
+		"name", name,
+		"id", id,
+		"message", message,
+		"method", r.Method,
+		"event_type", EventTypePlaySound.String(),
+		"remote_addr", r.RemoteAddr)
+
+	logger.Info("Sound play request",
+		"sfx_name", sfxName,
+		"name", name,
+		"id", id,
+		"message", message,
+		"event_type", EventTypePlaySound.String())
 
 	if sound, ok := P[n]; ok {
 		sound.Play()
+		fmt.Fprintf(w, "Playing sound: %s\n", n)
 	} else {
+		logger.Warn("Sound not found",
+			"sound_name", n,
+			"event_type", EventTypePlaySound.String())
 		http.Error(w, "Sound not found", http.StatusNotFound)
 		return
 	}
@@ -101,13 +199,12 @@ func HandlerSfx(w http.ResponseWriter, r *http.Request) {
 func onReady() {
 	iconBytes, err := e.Icon.ReadFile("icons/tray.ico")
 	if err != nil {
-		log.Fatalf("Failed to load icon: %v", err)
+		logger.Error("Failed to load icon", "error", err)
+		os.Exit(1)
 	}
-
 	systray.SetIcon(iconBytes)
 	systray.SetTitle("cybersonicd")
 	systray.SetTooltip("cybersonicd")
-
 	mQuit := systray.AddMenuItem("Quit", "Quit the application")
 	go func() {
 		<-mQuit.ClickedCh
@@ -116,7 +213,7 @@ func onReady() {
 }
 
 func onExit() {
-	log.Println("Stopping Cybersonicd...")
+	logger.Info("Stopping Cybersonicd...")
 	for _, sp := range P {
 		sp.Close()
 	}
@@ -125,16 +222,45 @@ func onExit() {
 
 func main() {
 	var (
-		address string
+		address      string
+		logLevelFlag string
 	)
+
 	flag.StringVar(&address, "address", "127.0.0.1:49161", "server address")
+	flag.StringVar(&logLevelFlag, "log-level", "info", "Logging level (debug, info, warn, error)")
 	flag.Parse()
+
+	opts := &slog.HandlerOptions{
+		Level: logLevel,
+	}
+
+	switch logLevelFlag {
+	case "debug":
+		logLevel.Set(slog.LevelDebug)
+	case "info":
+		logLevel.Set(slog.LevelInfo)
+	case "warn":
+		logLevel.Set(slog.LevelWarn)
+	case "error":
+		logLevel.Set(slog.LevelError)
+	default:
+		fmt.Fprintf(os.Stderr, "Invalid log level: %s. Using default (info)\n", logLevelFlag)
+		logLevel.Set(slog.LevelInfo)
+	}
+
+	jsonHandler := slog.NewJSONHandler(os.Stdout, opts)
+	logger = slog.New(jsonHandler)
+
+	slog.SetDefault(logger)
 
 	go func() {
 		http.HandleFunc("/sfx", HandlerSfx)
 		http.HandleFunc("/all", HandlerAll)
-		log.Printf("Cybersonicd is listening at http://%s ...\n", address)
-		http.ListenAndServe(address, nil)
+		logger.Info("Cybersonicd is listening", "address", address)
+		if err := http.ListenAndServe(address, nil); err != nil {
+			logger.Error("Server failed to start", "error", err)
+			os.Exit(1)
+		}
 	}()
 
 	systray.Run(onReady, onExit)
